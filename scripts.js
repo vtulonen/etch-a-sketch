445d4a21654b15f26435f97db835eca309fb16c7
Vili Tulonen <tulonenvili@gmail.com>
2019-10-30 21:28:41 +0200

    16x16 grid + mouseover for each square

--- scripts.js
+++ scripts.js
@@ -1,18 +1,51 @@
+window.onload = addGrid();
 
 function addGrid(){
-    let container = document.querySelector("#container");
+    let container = document.querySelector(".container");
     let content = document.createElement('div');
     content.classList.add('flex-container');
 
     
-    for (let i=0; i<16*16; i++){
-        let newDiv = document.createElement('div');
-        newDiv.id="sq";
-        newDiv.textContent = 1;
-        content.appendChild(newDiv);
+    for (let i=0; i<16; i++){
+        let row = document.createElement('div');
+        row.classList.add('row');
+        
+        for (let j=0; j<16; j++){
+            let box = document.createElement('div');
+            box.classList.add('box');
+            box.textContent = 1;
+            row.appendChild(box);
+        }
+        
+        content.appendChild(row);
     }
-    content.style.display = 'flex';
-    
     
     container.appendChild(content);
+
+    const boxes = document.querySelectorAll('.box');
+    boxes.forEach((box) =>{
+        box.addEventListener('mouseover', () =>{
+            console.log("hover");
+            box.style.background = 'black';
+        })
+    })
+
+
+
+/* if CSS needs to be added with DOM methods
+let style = document.createElement('style');
+style.innerHTML = `
+.row {
+    display: flex;
+}
+
+.box {
+    width: 50px;
+    height: 50px;
+    text-align: justify;
+}
+`;
+
+document.head.appendChild(style);
+*/
 }
