2ef23211dc6749bef2edb4cba2e51ba079b1a180
Vili Tulonen <tulonenvili@gmail.com>
2019-11-04 20:12:11 +0200

    redid loop to create grid boxes, changed flexbox css to make and added button to change size of grid

--- scripts.js
+++ scripts.js
@@ -1,51 +1,120 @@
 window.onload = addGrid();
 
+
+function clearGrid(){
+    const rmvnode = document.getElementById('fc');
+        while (rmvnode.firstChild){
+            rmvnode.removeChild(rmvnode.firstChild);
+        }
+}
+
+function addNewGrid(boxCount){
+    for (let j=0; j<boxCount; j++){
+        let box = document.createElement('div');
+        box.classList.add('box');
+        box.setAttribute('id', 'box');
+        //box.textContent = 1;
+        content.appendChild(box);
+    }
+    container.appendChild(content);
+}
+
+function changeDivColorHover(){
+    const boxes = document.querySelectorAll('.box');
+    boxes.forEach((box) =>{
+        box.addEventListener('mouseover', () =>{
+            box.style.background = 'black';
+            
+        })
+    })
+}
+
+
 function addGrid(){
     let container = document.querySelector(".container");
     let content = document.createElement('div');
     content.classList.add('flex-container');
-
-    
-    for (let i=0; i<16; i++){
-        let row = document.createElement('div');
-        row.classList.add('row');
-        
-        for (let j=0; j<16; j++){
+    content.setAttribute('id', 'fc');
+    let rowSize = 16; // to create 16 rows, 16 columns
+    let boxCount = rowSize * rowSize;
+        for (let j=0; j<boxCount; j++){
             let box = document.createElement('div');
             box.classList.add('box');
-            box.textContent = 1;
-            row.appendChild(box);
+            box.setAttribute('id', 'box');
+            //box.textContent = 1;
+            content.appendChild(box);
         }
         
-        content.appendChild(row);
-    }
     
     container.appendChild(content);
 
+
+    // change div color on mouseover
     const boxes = document.querySelectorAll('.box');
     boxes.forEach((box) =>{
         box.addEventListener('mouseover', () =>{
-            console.log("hover");
             box.style.background = 'black';
+            
         })
     })
 
+    // add a button on top of the page
+    let button = document.createElement('button');
+    button.textContent = 'New Grid';
+    container.insertBefore(button, container.firstChild);
+    
+    // reset box div colors on click of button
+    
+    let btn = document.querySelector('button');
+    
+    btn.addEventListener('click', () =>{
+        
+        
+        //add promt
+        newSize = prompt('Enter new row size for your grid:')
+        boxCount = newSize * newSize;
+        // newSize = set new box size
+        let newWnH = 640 / newSize;
+        console.log(boxCount);
+        
+        clearGrid();
+
+        //create new grid with newSize/boxcount
+        for (let j=0; j<boxCount; j++){
+            let box = document.createElement('div');
+            box.classList.add('box');
+            box.setAttribute('id', 'box');
+            box.style.width = newWnH +'px'; // new size
+            box.style.height = newWnH+'px'; // new height
+            //box.textContent = 1;
+            content.appendChild(box);
+        }
+        
+        boxes.forEach((box) =>{
+            
+        })
+
+    container.appendChild(content);
+    
+    changeDivColorHover();
+
+        
+    
+    })
+
+  
 
 
 /* if CSS needs to be added with DOM methods
 let style = document.createElement('style');
 style.innerHTML = `
-.row {
-    display: flex;
-}
 
-.box {
-    width: 50px;
-    height: 50px;
-    text-align: justify;
-}
+ .css rules {
+    something: something
+ }
 `;
 
 document.head.appendChild(style);
 */
 }
+
